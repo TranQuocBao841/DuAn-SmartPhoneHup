8ce5100e6da8b685b5556c673d66fcf9f6ed18e5
TranQuocBao841 <bao2004tk@gmail.com>
2025-03-20 00:24:52 +0700

    Delete SmartPhoneHup directory

--- SmartPhoneHup/src/main/java/com/example/SmartPhoneHup/DuAn/repository/HoaDonRepo.java
+++
@@ -1,9 +0,0 @@
-package com.example.SmartPhoneHup.DuAn.repository;
-
-import com.example.SmartPhoneHup.DuAn.model.HoaDon;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface HoaDonRepo extends JpaRepository<HoaDon,Integer> {
-}
